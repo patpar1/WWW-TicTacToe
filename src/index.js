a78ede903f8dc5d101029f6b7b4e16232f10badd
patpar1 <59889312+patpar1@users.noreply.github.com>
2020-06-19 14:47:29 +0300

    Added A2 functionality
    Added formatting and a timer.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "./styles.css";
 
-let width = 5;
-let height = 5;
+const board_size = 5;
+const players = ["x", "o"];
+const board = [];
 
-let board = [];
-let player1 = "x";
-let player2 = "o";
 let player1Turn = true;
 let moves = 0;
+let time = 10;
+let bar = null;
+let id = null;
 
 if (document.readyState !== "loading") {
   console.log("Document ready, executing");
@@ -20,249 +21,102 @@ if (document.readyState !== "loading") {
 }
 
 function init() {
-  for (var row = 0; row < height; row++) {
-    var tr = document.createElement("TR");
+  console.log("Initializing...");
+  for (let row = 0; row < board_size; row++) {
+    let tr = document.createElement("TR");
     board.push([]);
-    for (var col = 0; col < width; col++) {
-      var td = document.createElement("TD");
-      td.setAttribute("height", 60);
-      td.setAttribute("width", 60);
-      td.setAttribute("align", "center");
-      td.setAttribute("valign", "center");
-      td.addEventListener("click", makeMove);
+    for (let col = 0; col < board_size; col++) {
+      let td = document.createElement("TD");
+      td.addEventListener("click", event => {
+        makeMove(event.target);
+        event.stopPropagation();
+      });
       tr.appendChild(td);
       board[row].push(td);
     }
     document.getElementById("board").appendChild(tr);
   }
+  console.log("Board initialized!");
+  start_game();
 }
 
-function currentPlayer() {
-  return player1Turn ? player1 : player2;
+function start_game() {
+  bar = document.getElementById("progress_bar");
+  id = setInterval(frame, 1000);
+}
+
+function equals(array) {
+  return (
+    array[0].innerHTML !== "" &&
+    array.every(x => x.innerHTML === array[0].innerHTML)
+  );
 }
 
 function checkWin() {
-  var check;
-  var count = 0;
+  const d1 = [];
+  const d2 = [];
 
-  var row;
-  var col;
-
-  // Rows:
-  for (row = 0; row < height; row++) {
-    for (col = 0; col < width; col++) {
-      if (
-        board[row][col].innerHTML !== "" &&
-        board[row][col].innerHTML === check
-      ) {
-        count++;
-        if (count >= 5) {
-          return true;
-        }
-      } else {
-        check = board[row][col].innerHTML;
-        count = 1;
-      }
+  for (let i = 0; i < board_size; i++) {
+    if (equals(board[i])) {
+      return true;
     }
-    check = null;
-  }
 
-  check = null;
-
-  // Columns:
-  for (col = 0; col < width; col++) {
-    for (row = 0; row < height; row++) {
-      if (
-        board[row][col].innerHTML !== "" &&
-        board[row][col].innerHTML === check
-      ) {
-        count++;
-        if (count >= 5) {
-          return true;
-        }
-      } else {
-        check = board[row][col].innerHTML;
-        count = 1;
-      }
+    var col = [];
+    for (let j = 0; j < board_size; j++) {
+      col.push(board[j][i]);
     }
-    check = null;
-  }
-
-  check = null;
-
-  // Diagonals 1:
-  for (row = 2; row < height - 2; row++) {
-    for (col = 2; col < width - 2; col++) {
-      var i;
-
-      for (i = -2; i <= 2; i++) {
-        if (
-          board[row + i][col + i].innerHTML !== "" &&
-          board[row + i][col + i].innerHTML === check
-        ) {
-          count++;
-          if (count >= 5) {
-            return true;
-          }
-        } else {
-          check = board[row + i][col + i].innerHTML;
-          count = 1;
-        }
-      }
-      check = null;
+    if (equals(col)) {
+      return true;
     }
+
+    d1.push(board[i][i]);
+    d2.push(board[board_size - i - 1][i]);
   }
-
-  check = null;
-
-  // Diagonals 2:
-  for (row = 2; row < height - 2; row++) {
-    for (col = 2; col < width - 2; col++) {
-      for (i = -2; i <= 2; i++) {
-        if (
-          board[row + i][col - i].innerHTML !== "" &&
-          board[row + i][col - i].innerHTML === check
-        ) {
-          count++;
-          if (count >= 5) {
-            return true;
-          }
-        } else {
-          check = board[row + i][col - i].innerHTML;
-          count = 1;
-        }
-      }
-      check = null;
-    }
+  if (equals(d1) || equals(d2)) {
+    return true;
   }
-
   return false;
 }
 
-function expandBoard(i, j) {
-  var x;
-  var col;
-  var row;
-  var tr;
-  var td;
-
-  var tHeight;
-  var tWidth;
-
-  // Top
-  if (i < 3) {
-    tWidth = width;
-    for (x = 0; x < 3 - i; x++) {
-      tr = document.createElement("TR");
-      board.unshift([]);
-      height++;
-      for (col = 0; col < tWidth; col++) {
-        td = document.createElement("TD");
-        td.setAttribute("height", 60);
-        td.setAttribute("width", 60);
-        td.setAttribute("align", "center");
-        td.setAttribute("valign", "center");
-        td.addEventListener("click", makeMove);
-        tr.appendChild(td);
-        board[0].push(td);
-      }
-      document.getElementById("board").insertAdjacentElement("afterbegin", tr);
-    }
-  }
-
-  // Bottom
-  else if (i > height - 4) {
-    tHeight = height;
-    tWidth = width;
-    for (x = 0; x < i - (tHeight - 3) + 1; x++) {
-      tr = document.createElement("TR");
-      board.push([]);
-      height++;
-      for (col = 0; col < tWidth; col++) {
-        td = document.createElement("TD");
-        td.setAttribute("height", 60);
-        td.setAttribute("width", 60);
-        td.setAttribute("align", "center");
-        td.setAttribute("valign", "center");
-        td.addEventListener("click", makeMove);
-        tr.appendChild(td);
-        board[board.length - 1].push(td);
-      }
-      document.getElementById("board").appendChild(tr);
-    }
-  }
-
-  // Left
-  if (j < 3) {
-    tHeight = height;
-    tWidth = width;
-    for (x = 0; x < 3 - j; x++) {
-      width++;
-      for (row = 0; row < tHeight; row++) {
-        td = document.createElement("TD");
-        td.setAttribute("height", 60);
-        td.setAttribute("width", 60);
-        td.setAttribute("align", "center");
-        td.setAttribute("valign", "center");
-        td.addEventListener("click", makeMove);
-        document
-          .getElementById("board")
-          .getElementsByTagName("TR")
-          [row].insertAdjacentElement("afterbegin", td);
-        board[row].unshift(td);
-      }
-    }
-  }
-
-  // Right
-  else if (j > width - 4) {
-    tHeight = height;
-    tWidth = width;
-    for (x = 0; x < j - (tWidth - 3) + 1; x++) {
-      width++;
-      for (row = 0; row < tHeight; row++) {
-        td = document.createElement("TD");
-        td.setAttribute("height", 60);
-        td.setAttribute("width", 60);
-        td.setAttribute("align", "center");
-        td.setAttribute("valign", "center");
-        td.addEventListener("click", makeMove);
-        document
-          .getElementById("board")
-          .getElementsByTagName("TR")
-          [row].appendChild(td);
-        board[row].push(td);
-      }
-    }
+function frame() {
+  if (time === 0) {
+    player1Turn = !player1Turn;
+    reset_timer();
+  } else {
+    time--;
+    bar.style.width = 32 * time + "px";
+    bar.innerHTML = time + "s";
   }
 }
 
-function makeMove() {
-  if (this.innerHTML !== "") {
+function reset_timer() {
+  clearInterval(id);
+  time = 10;
+  id = setInterval(frame, 1000);
+  bar.style.width = "320px";
+  bar.innerHTML = "10s";
+  if (player1Turn) {
+    bar.className = "firstplayer";
+  } else {
+    bar.className = "secondplayer";
+  }
+}
+
+function makeMove(e) {
+  if (e.innerHTML !== "") {
     return;
   }
-  this.innerHTML = currentPlayer();
+  e.innerHTML = player1Turn ? players[0] : players[1];
+  e.className = player1Turn ? "firstplayer" : "secondplayer";
   moves++;
   if (checkWin()) {
     alert("Player " + (player1Turn ? "1" : "2") + " won!");
-  } else if (moves === width * height) {
+    clearInterval(id);
+  } else if (moves === board_size * board_size) {
     alert("Draw!");
+    clearInterval(id);
   } else {
     player1Turn = !player1Turn;
-
-    var i;
-    var j;
-
-    //Expand the game board:
-    for (var row = 0; row < height; row++) {
-      for (var col = 0; col < width; col++) {
-        if (board[row][col] === this) {
-          i = row;
-          j = col;
-        }
-      }
-    }
-
-    // expandBoard(i, j);
+    reset_timer();
   }
 }
